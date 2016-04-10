dcd6cafb61f3d1728aef9f347982f046eed44220
Максим Прокопьев <maxjazz@MacBook-Pro-Maksim.local>
2016-04-10 20:48:13 +0300

    first commit

---
+++ pinthing/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  pinthing
+//
+//  Created by Максим Прокопьев on 10.04.16.
+//  Copyright © 2016 M&O. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
