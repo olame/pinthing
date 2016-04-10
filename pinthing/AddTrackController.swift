1f3aca2549080327f9d39eeda7af8d686c177045
Максим Прокопьев <maxjazz@macbook-pro-maksim.local>
2016-04-10 23:48:13 +0300

    ui for add track

--- pinthing/AddTrackController.swift
+++ pinthing/AddTrackController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  AddTrackController.swift
 //  pinthing
 //
 //  Created by Максим Прокопьев on 10.04.16.
@@ -7,19 +7,16 @@
 //
 
 import UIKit
-
-class FirstViewController: UIViewController {
-
+class AddTrackController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
